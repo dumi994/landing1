35fbcc27e73f2ebf49cc9c74571aeded9d5aa7fa
Dumi <dumi.9944@gmail.com>
2023-04-02 19:40:32 +0200

    aggiunti orari e corretto form

--- src/components/main/Contacts/Contacts.js
+++ src/components/main/Contacts/Contacts.js
@@ -1,42 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layer from '../../Layer/Layer'
 import styles from "./Contacts.module.css"
+import Info from './Info'
+import Form from './Form'
+
 const Contacts = () => {
   return (
     <div className={`${styles.contacts}`}>
-    {/* <div className={styles.teamImg} style={{ backgroundImage:`url(${imgBg})`,backgroundSize:"cover"}}></div> */}
       <Layer>
-        <div className='container d-flex py-5'>
-          <div className='col-6 my-5'>
-            <ul>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              <li>Lorem ipsum</li>
-              
-            </ul>
-          </div>
-          <form className={`${styles.infoForm} col-6 my-5`}>
-            <div class="row my-4">
-              <div class="col">
-                <input type="text" class="form-control" placeholder="First name"/>
-              </div>
-              <div class="col">
-                <input type="text" class="form-control" placeholder="Last name"/>
-              </div>
-            </div>
-            <div className="row my-4">
-              <div className="col">
-                <input type="email" class="form-control" placeholder="Email"/>
-              </div>
-            </div>
-            <div className="row my-4">
-              <div className="col">
-                <textarea name="comment" class="form-control" form="usrform" placeholder="Messaggio"></textarea>
-              </div>
-            </div>
-          </form>
+        <div className='container row d-flex py-5'>
+        <h2 className='mb-5'>Contattaci</h2>
+          <Form></Form>
+          <Info></Info>
         </div>
       </Layer>
     </div>
